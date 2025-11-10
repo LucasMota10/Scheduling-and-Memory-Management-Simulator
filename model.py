c2d3efb0a66d805e3611e92f36f960b96ee37783
Lucas Barbosa Bulcão Mota <101370558+LucasMota10@users.noreply.github.com>
2025-11-10 18:20:14 -0300

    Add files via upload

--- model.py
+++ model.py
@@ -1,26 +1,58 @@
 import pandas as pd
 import numpy as np
+from abc import ABC, abstractmethod
 
-class simulator:
-    def __init__(self,quantum,overheat,disk_cost,processos):
+class Algorithms:
+    def __init__(self,quantum,overheat,disk_cost,process_list):
 
         self.quantum = quantum
         self.overheat = overheat
         self.disk_cost = disk_cost
-        self.processos = processos
+        self.process_list = process_list
 
-        self.processos_futuros = sorted(processos, key=lambda p: p.arrival)
-        self.ready_queue = []
+        self.ready_queue = sorted(process_list, key=lambda p: p.arrival)
+
         self.blocked_queue = []
         self.finished_process = []
 
         self.actual_time = 0
         self.actual_process = None
         self.idle_cpu = 0
-        self.pre_emption = 0
-
+        
         self.log = []
-class process:
+
+    @abstractmethod
+    def execute(self):
+        pass
+
+
+class Fifo(Algorithms):
+    def __init__(self):
+    
+    def execute(self):
+
+class Sjf(Algorithms):
+     def __init__(self):
+    
+    def execute(self):
+
+class Round_Robin(Algorithms):
+     def __init__(self):
+    
+    def execute(self):
+
+class EDF(Algorithms):
+     def __init__(self):
+    
+    def execute(self):
+
+class CFS_Sim(Algorithms):
+     def __init__(self):
+    
+    def execute(self):
+
+
+class Process:
     def __init__(self,id,arrival,total_time,priority,deadline,num_pages):
         self.id = id
         self.arrival = arrival
@@ -37,3 +69,4 @@ class process:
         self.wait_time = 0
         self.turnaround_time = 0
         self.finish_time = -1
+
